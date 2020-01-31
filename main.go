85b0bc74e80fb72c7882c85dfa79f6fbab38895e
SewellDinG <bigder.ding@gmail.com>
2020-01-31 15:22:22 +0800

    Fix panic:index out of range [1] with length 1

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 // Extended Ping
 
 import (
+    "bytes"
     "fmt"
     "log"
     "net/url"
@@ -14,56 +15,52 @@ import (
 
 func ePing(pingCount, pingHost string) error {
     cmd := exec.Command("ping", pingCount, pingHost)
-    //err := cmd.Run()
-    stdout, err := cmd.StdoutPipe()
-    cmd.Stderr = cmd.Stdout
-    if err != nil {
-        return err
-    }
-    if err = cmd.Start(); err != nil {
+    stdout := bytes.Buffer{}
+    cmd.Stdout = &stdout
+    if err := cmd.Start(); err != nil {
         return err
     }
     // realtime echo
-    for {
-        tmp := make([]byte, 1024)
-        _, err := stdout.Read(tmp)
-        fmt.Print(string(tmp))
-        if err != nil {
-            break
-        }
-    }
-    if err = cmd.Wait(); err != nil {
+    if err := cmd.Wait(); err != nil {
         return err
+    } else {
+        fmt.Println(stdout.String())
     }
     return nil
 }
 
 func main() {
     // eping https://baidu.com:443/urlPath/index.html
-    var pingUrl string = os.Args[1]
-    var pingCount string
-    if len(os.Args) > 2 {
-        fmt.Println("Please enter a parameter.")
+    if len(os.Args) < 1 {
+        fmt.Println("Please enter host.")
         os.Exit(0)
     }
-    // get host: baidu.com
-    if !strings.HasPrefix(os.Args[1], "http") {
-        pingUrl = "http://" + os.Args[1]
+    if len(os.Args) == 2 {
+        var pingUrl string = os.Args[1]
+        var pingCount string
+        // get host: baidu.com
+        if !strings.HasPrefix(os.Args[1], "http") {
+            pingUrl = "http://" + os.Args[1]
+        }
+        pingUrlInfo, err := url.Parse(pingUrl)
+        if err != nil {
+            log.Fatal("url.Parse err:", err)
+        }
+        pingHost := strings.Split(pingUrlInfo.Host, ":")[0]
+        // windows default ping 4 times
+        if runtime.GOOS != "windows" {
+            pingCount = "-c4"
+        } else {
+            pingCount = ""
+        }
+        // exec command
+        err = ePing(pingCount, pingHost)
+        if err != nil {
+            log.Fatal("func ePing err:", err)
+        }
     }
-    pingUrlInfo, err := url.Parse(pingUrl)
-    if err != nil {
-        log.Fatal("url.Parse err:", err)
-    }
-    pingHost := strings.Split(pingUrlInfo.Host, ":")[0]
-    // windows default ping 4 times
-    if runtime.GOOS != "windows" {
-        pingCount = "-c4"
-    } else {
-        pingCount = ""
-    }
-    // exec command
-    err = ePing(pingCount, pingHost)
-    if err != nil {
-        log.Fatal("func ePing err:", err)
+    if len(os.Args) > 2 {
+        fmt.Println("Please enter one parameter.")
+        os.Exit(0)
     }
 }
